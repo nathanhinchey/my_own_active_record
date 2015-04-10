e83707fc993f05c044e2f1512e990d89b445b97b
= <=>
2015-04-10 17:04:41 -0400

    Finish BelongsToOptions and HasManyOptions.

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,30 +10,42 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] ||= (name.to_s + "_id").to_sym
+    @primary_key = options[:primary_key] ||= (:id)
+    @class_name = options[:class_name] ||= name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] ||= (self_class_name.downcase + "_id").to_sym
+    @primary_key = options[:primary_key] ||= (:id)
+    @class_name = options[:class_name] ||= name.to_s.camelcase.singularize
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options.foreign_key
+      my_owner = options.model_class.where(id: options.foreign_key)
+      p "XXXXXXXXXXXXX"
+      p my_owner
+      return my_owner
+    end
   end
 
   def has_many(name, options = {})
@@ -46,5 +58,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
