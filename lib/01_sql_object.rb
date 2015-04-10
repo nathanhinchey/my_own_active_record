0e0bd517199a7f6c94c5a2c2a133769127c70cfb
App Academy Student <appacademy@192.168.1.235>
2015-04-10 13:48:55 -0400

    Initial commit

---
+++ lib/01_sql_object.rb
@@ -0,0 +1,77 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    query = <<-SQL
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    # p self.table_name
+    DBConnection.execute2(query).first.map(&:to_sym)
+  end
+
+  def self.finalize!
+    self.columns.each do |column|
+      define_method(column) do
+        attributes[column]
+      end
+
+      define_method("#{column}=") do |value|
+        attributes[column] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.name.downcase.pluralize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    params.each do |attr_name,attr_value|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise "unknown attribute #{attr_name}"
+      end
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
